2e35064026d06e8f4397672e1e8ff3d79a23e065
Benson Lee <BensonL1334@gmail.com>
2023-08-02 01:58:24 -0400

    DB code created will test later

--- Server/db/create_db.py
+++ Server/db/create_db.py
@@ -1,8 +1,30 @@
 import psycopg2
+import os
+
+
+# Load the environment variables
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_pw = os.environ.get('DB_PW')
 
 # Connect to postgres
-connection = psycopg2.connect()
+connection = psycopg2.connect(
+    dbname='postgres', user=db_user, password=db_pw,
+)
 
+# Create Cursor
+
+cursor = connection.cursor()
 
 # Create db
-# Disconnect
+
+cursor.execute(f"CREATE DATABASE {db_name}")
+
+# Commit the changes
+
+connection.commit()
+
+# Close cursor and disconnect
+
+cursor.close()
+connection.close()
