e15be00cfc64ef9ff5db71c3d2941c566b8567ad
Benson Lee <BensonL1334@gmail.com>
2023-08-03 15:25:01 -0400

    Create superuser file

---
+++ Server/db/create_superuser.py
@@ -0,0 +1,62 @@
+import psycopg2
+import os
+import hashlib
+
+# Load environment variables
+db_user = os.environ.get('DB_USER')
+db_pw = os.environ.get('DB_PW')
+
+
+def check_superuser_exists(username):
+    # Establish connection to postgres db
+    connection = psycopg2.connect(
+        dbname='postgres', name=db_user, password=db_pw,
+    )
+    # Create cursor
+    cursor = connection.cursor()
+
+    # Execute sql query to check if superuser already exists
+    cursor.execute(
+        "SELECT rolname FROM pg_roles WHERE rolname = %s", (username,))
+    superuser_exists = cursor.fetchone() is not None
+
+    cursor.close()
+    connection.close()
+
+    return superuser_exists
+
+
+def create_superuser(username, password):
+    # Establish connection to postgres db
+    connection = psycopg2.connect(
+        dbname='postgres', user=db_user, password=db_pw,
+    )
+    # Create cursor
+    cursor = connection.cursor()
+
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    try:
+        # Execute parameterized sql queries to create superuser
+        cursor.execute("CREATE USER %s WITH PASSWORD %s",
+                       (username, hashed_password,))
+        cursor.execute("ALTER USER %s WITH SUPERUSER", (username,))
+
+        # Commit the changes
+        connection.commit()
+        print(f"Superuser '{username}' created successfully.")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        connection.rollback()
+
+    cursor.close()
+    connection.close()
+
+
+superuser_username = os.environ.get('SUP_USER')
+superuser_password = os.environ.get('SUP_USERPW')
+
+if not check_superuser_exists(superuser_username):
+    create_superuser(superuser_username, superuser_password)
+else:
+    print(f"The superuser '{superuser_username}' already exists.")
