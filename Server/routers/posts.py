76e888b2366e7b9a18dc3f5b42dab15201690401
Benson Lee <BensonL1334@gmail.com>
2023-08-18 23:57:23 -0400

    Edited models

--- Server/routers/posts.py
+++ Server/routers/posts.py
@@ -1,1 +1,20 @@
 # CRUD for posts
+
+from flask import Blueprint, request, jsonify
+from datetime import datetime, timedelta
+from posts import Post
+
+posts_bp = Blueprint('posts', __name__)
+
+# Consider that you may be requesting:
+# Homepage (Your) posts
+# Someone else's posts
+# Specific post
+
+# Homepage posts
+@posts_bp.route('/posts', methods=['GET'])
+def get_posts():
+    try:
+        
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
