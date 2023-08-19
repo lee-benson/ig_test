ad9d1ce40d33e8703acfbd56df3d6612481c5e35
Benson Lee <BensonL1334@gmail.com>
2023-08-19 00:10:39 -0400

    Establish followers junction table

--- Server/models/followers.py
+++ Server/models/followers.py
@@ -12,3 +12,16 @@ db = PostgresqlDatabase(
     host='localhost',
     port=5432,
 )
+
+# Creates a relationship between followers and followees
+# To help establish homepage posts
+# Assume user1 is an instance of User
+# user1.following queries users that user1 is following (user1 is a follower)
+# user1.followers queries users that follow user1 (user1 is a followee)
+
+class follower(Model):
+    follower = ForeignKeyField(User, backref='following') 
+    followee = ForeignKeyField(User, backref='followers')
+
+    class Meta:
+        database = db
