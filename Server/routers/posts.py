2a407af5d510343c62ec38aee387d0f2d2eb7702
Benson Lee <BensonL1334@gmail.com>
2023-08-19 23:58:53 -0400

    Building out create post functionality

--- Server/routers/posts.py
+++ Server/routers/posts.py
@@ -5,6 +5,13 @@ from datetime import datetime, timedelta
 from posts import Post
 from ..models.users import User
 from ..models.followers import Follower
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN_KEY = os.environ.get('TOKEN_KEY')
 
 posts_bp = Blueprint('posts', __name__)
 
@@ -32,11 +39,48 @@ def get_posts():
 
         #  Return serialized data back to client
 
-        return jsonify([post.serialize() for post in posts])
+        return jsonify([post.serialize() for post in posts]), 200
+    except User.DoesNotExist:
+        return jsonify({'error' : 'User not found'}), 404
     except Exception as e:
         return jsonify({'error' : str(e)}), 500
     
 
-@posts_bp.route('/:username', methods=['GET'])
+# Someone else' posts
+@posts_bp.route('/<username>', methods=['GET'])
+def get_user_post():
+    try:
+        username = request.args.get("username")
+
+        user = User.select().where(User.username == username).get()
+
+        posts = Post.select().where(Post.user << user).order_by(Post.timestamp.desc())
+
+        return jsonify({[post.serialize() for post in posts]}), 200
+    except User.DoesNotExist:
+        return jsonify({'error' : 'User not found'}), 404
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
+
+# Create post
+@posts_bp.route('/', methods=['POST'])
+def create_post():
+    try:
+        
+        # Verify first (Will have a separate middleware verification as well)
+
+        data = request.json
+        auth_header = request.headers.get('Authorization')
+
+        if not auth_header:
+            return jsonify({'error' : 'Missing authorization header'}), 401
+        
+        token = auth_header.split()[1] 
+        decoded_token = jwt.verify(token, TOKEN_KEY)
+        user = User.get(User.username == decoded_token.username)
+
+        return jsonify(), 200
+    except Exception as e: 
+        return jsonify({'error' : str(e)}), 500
 
 
