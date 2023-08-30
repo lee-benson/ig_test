7008d6acf23bbda360ba0ca7e4109177b0fb55ad
Benson Lee <BensonL1334@gmail.com>
2023-08-29 22:39:19 -0400

    Add middleware function

--- Server/api/app.py
+++ Server/api/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from ..models.createTables import db
+from ..routers import auth_bp, users_bp, posts_bp, comments_bp, messages_bp, chatrooms_bp
+from ..middleware import verify_auth
+
 
 app = Flask(__name__)
 
+# Register blueprints
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.register_blueprint(auth_bp)
+app.register_blueprint(users_bp)
+app.register_blueprint(posts_bp)
+app.register_blueprint(comments_bp)
+app.register_blueprint(messages_bp)
+app.register_blueprint(chatrooms_bp)
+
+@app.before_request
+def before_request():
+    if request.endpoint in ['CREATE', 'PUT', 'DELETE']:
+        verify_auth()
 
 
 if __name__ == "__main__":
---
+++ Server/middleware/verifyAuth.py
@@ -0,0 +1,23 @@
+from flask import request, jsonify
+from ..models.users import User
+from dotenv import load_dotenv
+import os
+import jwt
+
+load_dotenv()
+
+TOKEN_KEY = os.environ.get('TOKEN_KEY')
+
+def verify_Auth():
+    try:
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return jsonify({'error': 'Missing authorization header'}), 401
+        token = auth_header.split()[1]
+        decoded_token = jwt.decode(token, TOKEN_KEY, algorithms=['HS256'])
+        user = User.get(User.id == decoded_token['user_id'])
+        if not user:
+            return jsonify({'message' : 'You need to be signed in to make this request'}), 401
+    except Exception as e:
+        return jsonify({'error : str(e)'}), 500
+        
